459a5f51c6a545d5acc07555f651b9eb10139f7e
jarekpavlov <jarekpavlov@gmail.com>
2020-06-16 15:27:38 +0300

    Creating UserDetail service object in WebSecurityConfig, UserRepository interface, to get user that logging in, converting User object to UserDetails object, creating new table ising @Entity to stor new Authorities field

--- FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfig.java
+++ FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfig.java
@@ -1,16 +1,21 @@
 package com.freshvotes.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		
@@ -21,17 +26,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		auth.inMemoryAuthentication()
-		.passwordEncoder(getPasswordEncoder())
-		.withUser("yaroslavvl")
-		.password(getPasswordEncoder().encode("asdf"))
-		.roles("USER");
+		auth.userDetailsService(userDetailsService)
+		.passwordEncoder(getPasswordEncoder());
+
+		//		auth.inMemoryAuthentication()
+//		.passwordEncoder(getPasswordEncoder())
+//		.withUser("yaroslavvl")
+//		.password(getPasswordEncoder().encode("asdf"))
+//		.roles("USER");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http//.csrf().disable()
+		http                  //.csrf().disable()
 		.authorizeRequests()
 		.antMatchers("/").permitAll()
 		.anyRequest().hasRole("USER").and()
