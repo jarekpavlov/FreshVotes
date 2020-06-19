668f3d4300746817b2691619f76c41f029e8b853
jarekpavlov <jarekpavlov@gmail.com>
2020-06-19 18:28:56 +0300

    creating UserService class to bind date to DB using UsingRepository class

--- FreshVotes/src/main/java/com/freshvotes/webcontrollers/LoginController.java
+++ FreshVotes/src/main/java/com/freshvotes/webcontrollers/LoginController.java
@@ -1,15 +1,20 @@
 package com.freshvotes.webcontrollers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.freshvotes.domain.User;
+import com.freshvotes.service.UserService;
 
 
 @Controller
 public class LoginController {
+	
+	@Autowired
+	UserService userService;
 	
 	//@RequestMapping(value="/login", method = RequestMethod.GET) =>
 	@GetMapping(value = "/login")
@@ -25,11 +30,9 @@ public class LoginController {
 	}
 	
 	@PostMapping(value = "/registration")
-	public String createAccount(User user) {
-		
-		System.out.println(user);
-		
-		return "redirect:/registration";
+	public String createAccount(User user) {	
+		userService.save(user);
+		return "redirect:/login";
 	}
 
 }
