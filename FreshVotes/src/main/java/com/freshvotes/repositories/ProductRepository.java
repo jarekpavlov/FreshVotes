b47ee8defdd470222e38543c0a5355f555ee1385
Jarek <jarekpavlov@gmail.com>
2020-06-27 22:02:33 +0300

    Creating custom query in repository of product using @Query to enable possibility fetch data from user object that has LAZY connection

--- FreshVotes/src/main/java/com/freshvotes/repositories/ProductRepository.java
+++ FreshVotes/src/main/java/com/freshvotes/repositories/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.freshvotes.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.freshvotes.domain.Product;
 import com.freshvotes.domain.User;
@@ -11,6 +13,9 @@ import com.freshvotes.domain.User;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
-	 List<Product> findByUser(User user);
+	@Query("select prod from Product prod join fetch prod.user where prod.id = :id")
+	Optional<Product> findByIdWithUser(Long id);
+	
+	List<Product> findByUser(User user);
 
 }
